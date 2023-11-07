c6a77a21e5765a75de995c7096a5d244a58a21bd
elpx <elpxhhh@gmail.com>
2023-11-07 23:14:05 +0800

    rename & modify system path

--- QLDependencyInstall.sh
+++ QLDependencyInstall.sh
@@ -48,7 +48,7 @@ pnpm install -g js-base64 qrcode-terminal silly-datetime
 
 pip3 install requests lxml
 
-cd /ql/scripts/ && apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev && npm i && npm i -S ts-node typescript @types/node date-fns axios png-js canvas --build-from-source
+cd /ql/data/scripts/ && apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev && npm i && npm i -S ts-node typescript @types/node date-fns axios png-js canvas --build-from-source
 cd /ql
 apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev && cd scripts && npm install canvas --build-from-source
 cd /ql
@@ -57,7 +57,5 @@ apk add python3 zlib-dev gcc jpeg-dev python3-dev musl-dev freetype-dev
 echo
 TIME g "依赖安装完毕...建议重启 Docker "
 
-echo
-TIME g "有任何问题，请在此仓库提交Issue： https://github.com/FlechazoPh/QLDependency"
 echo
 exit 0
